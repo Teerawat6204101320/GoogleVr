39f09353333d2a99ad5241b594f263d8e8d3eb0c
teerawat loawang <teerawatloawang@gmail.com>
2022-02-27 21:08:20 +0700

    first commit

---
+++ Assets/ChangAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void getAnimatorDefeate(){
+       this.GetComponent<Animator>().Play("Chicken Dance");
+   }
+    public void getAnimatorRumba(){
+       this.GetComponent<Animator>().Play("Snake Hip Hop Dance");
+   }
+
+    public void getAnimatorstand(){
+       this.GetComponent<Animator>().Play("mixamo_com");
+   }
+    public void getAnimatorrun(){
+       this.GetComponent<Animator>().Play("mixamo_com1");
+   }
+}
---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+    public void gotRed(){
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void gotBlue(){
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void gotGreen(){
+        this.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
